1a73141d21e6c65f658bb4092f0056f19b18d51e
JaeHoon <52474292+fofo0623@users.noreply.github.com>
2021-10-04 23:39:09 +0900

    Create nhn_pre_1.py
    2021.10.04 문제풀이

---
+++ python/nhn_pre_1.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+board = []
+cnt = 0
+size = []
+max_size = 0
+for _ in range(n):
+    board.append(list(map(int, sys.stdin.readline().split())))
+
+dr = [[0,1],[0,-1],[1,0],[-1,0]]
+
+def dfs(y,x):
+    global board, size, max_size
+
+    for dy, dx in dr:
+        ny, nx = y+ dy, x + dx
+        if 0 <= ny < n and 0 <= nx < n:
+            if board[ny][nx] == 1:
+                board[ny][nx] = -1
+                max_size += 1
+                dfs(ny, nx)
+
+    return True
+
+for y in range(n):
+    for x in range(n):
+        if board[y][x] == 1:
+            max_size = 0
+            check = dfs(y, x)
+            if check:
+                cnt += 1
+                size.append(max_size)
+
+print(cnt)
+print(sorted(size))
+
