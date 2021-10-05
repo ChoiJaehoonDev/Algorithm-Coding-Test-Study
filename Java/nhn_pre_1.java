9856577860237f84725c269850bb9e203db731e2
JaeHoon <52474292+fofo0623@users.noreply.github.com>
2021-10-05 23:25:18 +0900

    Update nhn_pre_1.java

--- Java/nhn_pre_1.java
+++ Java/nhn_pre_1.java
@@ -1,15 +1,27 @@
 package Java;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class nhn_pre_1{
+
+    public boolean dfs(int y, int x){
+        return false;
+
+    }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int i = 0;
+        int j = 0;
         int n = scanner.nextInt();
+        int [][]board = new int[10][10];
+
         
         for(i=0; i<n; i++){
-            
+            for (j=0; j< n; j++){
+                board[i][j] = scanner.nextInt();
+            }
         }
+    }
+
 }
