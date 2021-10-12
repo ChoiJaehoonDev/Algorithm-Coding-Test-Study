e14749e4ea35b8e0298ac175950937a70f8fc082
JaeHoon <52474292+fofo0623@users.noreply.github.com>
2021-10-12 21:26:53 +0900

    1475_방번호

---
+++ python/1475_방번호.py
@@ -0,0 +1,23 @@
+import sys
+from math import ceil
+N = sys.stdin.readline()
+pn = 0
+
+check = {}
+
+for n in N:
+    if n == '6' or n == '9':
+        n = '6'
+        if n not in check.keys():
+            check[n] = 0.5
+        else:
+            check[n] += 0.5
+
+    else:
+        if n not in check.keys():
+            check[n] = 1
+        else:
+            check[n] += 1
+
+print(check)
+print(ceil(max(check.values())))
